5f15813be6e7bb59ce369217bf0edabb972d1be7
mlgvla <mlgerardvla@gmail.com>
2020-09-20 15:19:02 -0400

    Created Sessions controller and connected to Welcome Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+    root 'sessions#welcome'
+    
+    # get 'sessions/welcome'
+    # get 'sessions/new'
+    # get 'sessions/create'
+    # get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
