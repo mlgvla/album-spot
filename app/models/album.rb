aaa70e5c979f2f2cb29fdd07fd951ee147c66e0c
mlgvla <mlgerardvla@gmail.com>
2020-09-22 01:24:24 -0400

    Added associations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,7 @@
 class Album < ApplicationRecord
-  belongs_to :artist
+    belongs_to :artist
+    # has_many :user_albums
+    # has_many :users, through: :user_albums
+    # has_many :tracks
+    # has_many :reviews, through: :user_albums
 end
