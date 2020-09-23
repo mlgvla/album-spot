369083f509e1d9a706fa6bee2039a879fdae5721
mlgvla <mlgerardvla@gmail.com>
2020-09-22 20:23:08 -0400

    Completed Sign Up methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+    # visible to views
+    helper_method :current_user, :logged_in?
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in # Should I expand and add a flash message?
+        redirect_to root_path if !logged_in
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,27 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            flash[:error] = "Account creation failed:  #{@user.errors.full_messages.to_sentence}."
+            render :new 
     end
 
     def show
-        
+        redirect_if_not_logged_in
+        @user = User.find_by_id(params[:id])
+        redirect_to root_path if !@user
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end 
+
 end
+
+
