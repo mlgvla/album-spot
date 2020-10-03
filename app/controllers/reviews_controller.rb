ee461e2cf5accfadf52106257fdc41bed8a4f163
mlgvla <mlgerardvla@gmail.com>
2020-10-03 01:12:34 -0400

    Working on getting nested form correct

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,9 @@ class ReviewsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
     def new
-        @review = Review.new
-        @user_album = UserAlbum.find_by(user_id: current_user.id, album_id: params[:album_id])       
+        
+        @user_album = UserAlbum.find_by(user_id: current_user.id, album_id: params[:album_id]) 
+        @review = Review.new #I think I have to build the review onto the user_album.      
     end
 
     def create
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user_album  
 
   accepts_nested_attributes_for :user_album
+
+  
 end
