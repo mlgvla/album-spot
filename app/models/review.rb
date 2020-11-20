af2a7d256c17f2f3a9a0811c1e64be1d755ae772
mlgvla <mlgerardvla@gmail.com>
2020-11-20 00:58:15 -0500

    Added stars and content validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,9 @@
 class Review < ApplicationRecord
     belongs_to :user_album
   
-  
- 
-
     validates :user_album, uniqueness: true #add a message?
+    validates :stars, presence: true, inclusion: { in: 0..5, message: "must have rating from 0 to 5" }
+    validates :content, length: { minimum: 10}, allow_blank: true    
 
     def user_album_attributes=(user_album_attributes)
         self.user_album_id = user_album_attributes["user_album_id"]
@@ -12,7 +11,7 @@ class Review < ApplicationRecord
     end
 
     
-
+# message: "Please rate the album out of 5"
 
   
 end
